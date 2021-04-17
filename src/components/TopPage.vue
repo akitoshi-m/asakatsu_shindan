f572cb748d83450188f00188d84c36c62fc9b7b8
github Toshimatu <matsuda.akt@gmail.com>
2021-04-18 07:58:32 +0900

    [Add] 朝活診断トップページの作成

---
+++ src/components/TopPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class='main-title'>＼\朝活診断/／</div>
+    <div class='sub-title'>
+      あなたの朝活力を診断します<br>
+      質問は10個。あなたの朝活力はいかに？
+    </div>
+    <img src="@/assets/asakatsu_shindan_top.svg" alt="asakatsu_shindanトップ画像" width="400px" height="400px" />
+    <div class="buttons">
+      <button class="button is-primary">診断を開始する</button>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.main-title {
+  color: #e25656;
+  font-size: 60px;
+}
+
+.sub-title {
+  font-size: 30px;
+}
+
+.button {
+  margin: 50px auto;
+}
+</style>
